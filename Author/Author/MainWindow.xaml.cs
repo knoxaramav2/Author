3bc905b84b225491b156a3c9de9357c0ee8e8e13
knoxaramav2 <wtscott@iastate.edu>
2020-04-24 23:53:52 -0500

    Tab button stubs

--- Author/Author/MainWindow.xaml.cs
+++ Author/Author/MainWindow.xaml.cs
@@ -24,8 +24,6 @@ namespace Author
     public partial class MainWindow : Window
     {
         #region private fields
-        private ViewTabHandler _tabHandler;
-
 
         #endregion
 
@@ -38,13 +36,14 @@ namespace Author
 
         private void InitializeGlobals()
         {
-            _tabHandler = new ViewTabHandler();
         }
 
         private void InitializeGui()
         {
             GeneralTab.Focus();
         }
+
+        #region TabSwitch
 
         private void ActorViewSelected(object sender, RoutedEventArgs e)
         {
@@ -65,6 +64,32 @@ namespace Author
         {
             DataContext = new RelationsViewModel();
         }
+
+        #endregion
+
+        #region AddTabItem
+
+        private void AddNewFact(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddNewActor(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddNewGeneral(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddNewRelation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
     }
 
     public class GraphControl : INotifyPropertyChanged
